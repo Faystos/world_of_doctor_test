4c3bcd450efb85e4d25593859383668198ebf15e
Vladimir <fayst_@bk.ru>
2020-10-01 18:43:09 +0300

    Добавил ещё пару тестов.

--- src/componets/SearchPanel/SearchPanel.test.js
+++ src/componets/SearchPanel/SearchPanel.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import SearchPanel from './SearchPanel';
+import ReactTestUtils from 'react-dom/test-utils';
 
 test('test SearchPanel', () => {
   const component = renderer.create(
@@ -9,5 +10,25 @@ test('test SearchPanel', () => {
 
   let tree = component.toJSON();
   expect(tree).toMatchSnapshot();  
+});
+
+describe('SearchPanel Component', () => {
+  it('has an section tag', () => {
+    const component = ReactTestUtils.renderIntoDocument(<SearchPanel/>);    
+    let section = ReactTestUtils.findRenderedDOMComponentWithTag(
+     component, 'section');
+   });   
+  
+   it('is wrapped inside a searchPanel class', () => {
+    const component = ReactTestUtils.renderIntoDocument(<SearchPanel/>);    
+    let classN = ReactTestUtils.findRenderedDOMComponentWithClass(
+    component, 'searchPanel');
+   });
+
+   it('is wrapped inside a blockInp class', () => {
+    const component = ReactTestUtils.renderIntoDocument(<SearchPanel/>);    
+    let classN = ReactTestUtils.findRenderedDOMComponentWithClass(
+    component, 'blockInp');
+   });
 });
 
